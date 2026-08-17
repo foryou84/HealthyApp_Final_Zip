a31b8f9c961fed5f1ae34f4ab0cea9d9ee300616
DAVID SH <foryou84.david@gmail.com>
2026-08-18 00:55:34 +0300

    temporary

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,1 @@
 const CACHE_NAME='healthy-app-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg'])));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
