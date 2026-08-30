ab9639c2d675ce81a2c685315272f4bb83f82b76
DAVID SH <foryou84.david@gmail.com>
2026-08-30 09:47:08 +0300

    Add monthly meal tracking and export

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='healthy-app-v47-weekly-day-details';
+const CACHE_NAME='healthy-app-v48-monthly-export';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./meal-table-fix.js'];
 
 self.addEventListener('install',event=>{
