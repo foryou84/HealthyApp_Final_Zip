56423ee35291933ea0839a6a7ee2fce825e9b49d
DAVID SH <foryou84.david@gmail.com>
2026-09-15 16:14:49 +0300

    Allow editing foods from the past week

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='healthy-app-v57-cloud-size-fix';
+const CACHE_NAME='healthy-app-v58-history-food-edit';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./meal-table-fix.js'];
 
 self.addEventListener('install',event=>{
