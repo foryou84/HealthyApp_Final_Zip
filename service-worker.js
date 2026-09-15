c8e379235b4eb31fe565b45bf518fa6152b34d63
DAVID SH <foryou84.david@gmail.com>
2026-09-15 18:52:56 +0300

    Make dietitian Excel export self contained

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='healthy-app-v60-dietitian-xlsx';
+const CACHE_NAME='healthy-app-v61-dietitian-excel';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./meal-table-fix.js'];
 
 self.addEventListener('install',event=>{
