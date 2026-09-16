f19399b6fd1585fd07c3f0611cd29904da702a97
DAVID SH <foryou84.david@gmail.com>
2026-09-16 14:29:14 +0300

    Add period reports with daily activity

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='healthy-app-v61-dietitian-excel';
+const CACHE_NAME='healthy-app-v62-analysis-activity-report';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./meal-table-fix.js'];
 
 self.addEventListener('install',event=>{
